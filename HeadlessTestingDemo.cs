9f5c2ec48b7703b1a72df289e087933a5ce773c7
Levi Wilson <levi@leviwilson.com>
2011-11-04 10:16:02 -0400

    separated test runner into Noesis.Javascript.Headless as a submodule, updated the example to use it.

---
+++ HeadlessTestingDemo.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Noesis.Javascript.Headless;
+using Noesis.Javascript.Headless.Reporters;
+
+namespace HeadlessTestingDemo
+{
+    [TestFixture]
+    public class HeadlessTestingDemo
+    {
+        [Test]
+        public void Then_all_of_our_specs_will_pass()
+        {
+            var testRunner = new JavaScriptTestRunner();
+            testRunner.Include(JavaScriptLibrary.jQuery_1_6_4_min);
+            testRunner.Include(JavaScriptLibrary.Jasmine_1_1_0);
+
+            // load the "Roman.js" file from the resource specified in <T>, which happens to be this assembly but could be in another project.
+            testRunner.LoadFromResource<HeadlessTestingDemo>("Roman.js");
+            testRunner.LoadFromResource<HeadlessTestingDemo>("RomanSpec.js");
+            testRunner.LoadFromResource<HeadlessTestingDemo>("SpecHelper.js");
+
+            var stringReporter = new StringReporter();
+
+            testRunner.RunJasmineSpecs(stringReporter);
+
+            Assert.That(stringReporter.TotalFailures, Is.EqualTo(0), stringReporter.Result);
+        }
+    }
+}
